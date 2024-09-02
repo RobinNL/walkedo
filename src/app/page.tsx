37907e6d9049e49ed2fdc079b333ac609b204356
Robin Chung <robinchung@me.com>
2024-09-02 17:31:01 +0200

    Homepage v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,13 @@ export default function Home() {
 
     return (
         <div>
-            <div className={Styles.heroImage} style={{height: width.current > 1920 ? 1080 : width.current * 0.5625 + 'px'}}>
-                <video width={width.current > 1920 ? '1920px' : width.current + 'px'} height={(width.current > 1920 ? '1080px' : width.current * 0.5625) + 'px'}
+            <div className={Styles.heroImage}
+                 style={{height: width.current > 1920 ? 1080 : width.current * 0.5625 + 'px'}}>
+                <video width={width.current > 1920 ? '1920px' : width.current + 'px'}
+                       height={(width.current > 1920 ? '1080px' : width.current * 0.5625) + 'px'}
                        className={Styles.heroVideo} controls={false} controlsList={"nodownload"} loop={true}
                        muted={true} preload="auto" autoPlay={true}>
-                    <source src="/videos/walkedo-introduction.mp4" type="video/mp4"/>
+                    <source src="/videos/walkedo-intro.mp4" type="video/mp4"/>
                     <Image sizes='max-width: 100vw' className={Styles.heroImageInner} fill={true}
                            src={'/images/walking-dogs-hero.jpg'}/>
                 </video>
@@ -29,25 +31,26 @@ export default function Home() {
             <div className={Styles.heroSignupBtn}>
                 <div className={Styles.heroSignupInner}>
                     <h2 className={Styles.heroSignupTitle}>Met hart voor je hond</h2>
-                    <p className={Styles.heroSignupDescription}>Laat je viervoeter de mooiste plekjes van Arnhem ontdekken met een groep waar je hond zich helemaal thuis
+                    <p className={Styles.heroSignupDescription}>Laat je viervoeter de mooiste plekjes van Arnhem
+                        ontdekken met een groep waar je hond zich helemaal thuis
                         voelt. En ga met een gerusthart op vakantie met de opvangservice.</p>
-                    <div className={Styles.heroSignupInnerBtn}>
-                        <WalkedoButton label={'Aanmelden'} disabled={false}/>
+                    <div className={Styles.heroSignupIconWrapper}>
+                        <Image className={Styles.heroSignupIconIcon} src={'fonts/icons/chevron-down.svg'} width={30}
+                               height={30}/>
+                        <Image className={`${Styles.heroSignupIconIcon} ${Styles.heroSignupIconIconBottom}`}
+                               src={'fonts/icons/chevron-down.svg'} width={30} height={30}/>
                     </div>
                 </div>
             </div>
             <div className={Styles.pageContainer}>
                 <div className={Styles.container}>
 
-                    <h1 className={Styles.indexHeader}>Hondenservices in Arnhem noord</h1>
+                    <h1 className={Styles.indexHeader}>Hondenservice Arnhem</h1>
 
-                    <p className={Styles.indexTextWrap}>Walkedo is een hondenuitlaatservice, kennel en meer op een
-                        adres. Een contactpunt met
-                        persoonlijke aandacht voor je hond. Zo loopt je hond in kleine groepen,
-                        en is er regelmatig plek voor (langdurige) opvang van je hond als je op vakantie
-                        gaat. Ontdek onze services en neem vrijblijvend contact op voor een kennismakingsgesprek.
-                        Ik leer je hond graag beter kennen.
-                    </p>
+                    <h3 className={Styles.indexDescriptionWrapper}>
+                        Ontdek waar je interesse in hebt en meld je aan voor een vrijblijvende kennismaking. Jouw hond
+                        is mijn klant en ik kan niet wachten op onze eerste kennismaking.
+                    </h3>
 
                 </div>
 
@@ -56,54 +59,107 @@ export default function Home() {
                     <div className={Styles.serviceSection}>
 
                         <div className={Styles.serviceTextSection}>
-                            <h3>Uitlaten</h3>
+                            <div className={Styles.indexServiceDescription}>
+                                <h2 className={Styles.serviceHeader}>Uitlaatservice</h2>
+                                <h3>Lekker de energie kwijt en socialiseren met andere honden.</h3>
 
-                            <p className={Styles.serviceTextBlock}>Laat je hond een of meerdere malen per week door
-                                mij ophalen. We lopen een uur met een
-                                groep
-                                van maximaal 6 andere honden die goed bij je eigen hond past.
-                                Na de wandeling breng ik je hond weer terug. Een uitlaatservice geeft je hond kans
-                                om
-                                lekker
-                                te spelen.</p>
-                            <Link className={Styles.learnMoreLink} href={'/uitlaatservice'}>Ontdek de
-                                Uitlaatservice</Link>
+                                <p>Laat je hond een of meerdere malen per week
+                                    door
+                                    mij ophalen. We lopen een uur met een groep van maximaal 6 andere honden die
+                                    goed bij je eigen hond passen. Zo heb ik een speciale Labradoodle groep, "de
+                                    walkadoodles", en een puppy groep op vrijdag.
+                                    Na de wandeling breng ik je hond weer terug. Een uitlaatservice geeft je hond
+                                    kans om lekker te spelenx en te socialiseren.</p>
+                                <Link className={Styles.learnMoreLink} href={'/uitlaatservice'}>
+                                    <span className={Styles.learnMoreLinkText}>
+                                        Uitlaatservice Arnhem Noord
+                                    </span>
+                                    <Image className={Styles.learnMoreLinkIcon} src={'/fonts/icons/arrow-right.svg'}
+                                           height={20} width={20}/>
+                                </Link>
+                            </div>
                         </div>
                     </div>
-
-                    <div className={Styles.serviceSection}>
-
-                        <div className={Styles.serviceTextSection}>
-                            <h3>Northern Inuit Dog</h3>
-
-                            <p className={Styles.serviceTextBlock}>Dit unieke ras is bij Walkedo als eerste in
-                                Nederland. Ik verwacht een groep pups te
-                                hebben in de toekomst. Leer dit unieke ras kennen en schrijf je vrijblijvend in voor
-                                de
-                                wachtlijst. Zodra het concreet wordt heb je dan als eerste de kans om een plekje te
-                                reserveren en eigenaar te worden van een beste vriend uit dit unieke ras!</p>
-                            <Link className={Styles.learnMoreLink} href={'/northern-Inuit-dog'}>Ontdek de Northern
-                                Inuit Dog</Link>
-                        </div>
+                </div>
+                <div className={Styles.serviceSectionImageRow}>
+                    <div className={Styles.serviceSectionImage}>
+                        <Image objectFit={'cover'} fill={true}
+                               src={'/images/uitlaatservice/uitlaatservice-arnhem.jpeg'}
+                               alt={'hond in het gras'}/>
                     </div>
-
-                    <div className={Styles.serviceSection}>
-
-                        <div className={Styles.serviceTextSection}>
-                            <h3>Opvang</h3>
-
-                            <p className={Styles.serviceTextBlock}>Iedereen gaat wel eens op vakantie of is een hele
-                                dag weg voor het werk. Klanten van Walkedo kunnen gebruik maken van de dagopvang en
-                                hun honden lekker laten spelen en rasen in een kennel van 25 vierkantemeters. De
-                                honden van de dagopvang gaan automatisch mee met de uitlaatservice. Meld je vandaag
-                                nog aan voor een gezellige kennismaking!</p>
-                            <Link className={Styles.learnMoreLink} href={'/opvang'}>Ontdek opvang</Link>
-                        </div>
+                    <div className={Styles.serviceSectionImage}>
+                        <Image objectFit={'cover'} fill={true}
+                               src={'/images/uitlaatservice/uitlaatservice-arnhem-2.jpeg'}
+                               alt={'hond in het gras'} className={Styles.serviceSectionImage}/>
                     </div>
-
+                    <div className={Styles.serviceSectionImage}>
+                        <Image objectFit={'cover'} fill={true}
+                               src={'/images/uitlaatservice/uitlaatservice-arnhem-3.jpeg'}
+                               alt={'hond in het gras'} className={Styles.serviceSectionImage}/>
+                    </div>
                 </div>
 
+                <div className={Styles.container}>
+
+                    <div className={Styles.serviceSection}>
+
+                        <div className={Styles.serviceTextSection}>
+                            <div className={Styles.indexServiceDescription}>
+                                <h2 className={Styles.serviceHeader}>Opvangservice</h2>
+                                <h3>Voor een dag, een week of een vakantieperiode.</h3>
+
+                                <p className={Styles.serviceTextBlock}>Iedereen gaat wel eens op vakantie of is een hele
+                                    dag weg voor het werk. Klanten van Walkedo kunnen gebruik maken van de dagopvang en
+                                    hun honden lekker laten spelen en rasen in een kennel van 25 vierkantemeters. De
+                                    honden van de dagopvang gaan automatisch mee met de uitlaatservice. Meld je vandaag
+                                    nog aan voor een gezellige kennismaking!</p>
+                                <Link className={Styles.learnMoreLink} href={'/opvang'}>
+                                     <span className={Styles.learnMoreLinkText}>
+                                        Opvangservice
+                                    </span>
+                                    <Image className={Styles.learnMoreLinkIcon} src={'/fonts/icons/arrow-right.svg'}
+                                           height={20} width={20}/>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className={Styles.container}>
+
+                    <div className={Styles.serviceSection}>
+
+                        <div className={Styles.serviceTextSection}>
+                            <div className={Styles.indexServiceDescription}>
+                                <h2 className={Styles.serviceHeader}>Northern Inuit Dog</h2>
+                                <h3>Een uniek ras bekend uit Game of Thrones en nu in Nederland.</h3>
+
+                                <p className={Styles.serviceTextBlock}>Dit unieke ras is bij Walkedo als eerste in
+                                    Nederland. Ik verwacht een groep pups te
+                                    hebben in de toekomst. Leer dit unieke ras kennen en schrijf je vrijblijvend in voor
+                                    de
+                                    wachtlijst. Zodra het concreet wordt heb je dan als eerste de kans om een plekje te
+                                    reserveren en eigenaar te worden van een beste vriend uit dit unieke ras!</p>
+                                <Link className={Styles.learnMoreLink} href={'/northern-Inuit-dog'}>
+                                     <span className={Styles.learnMoreLinkText}>
+                                        Northern Inuit Dog
+                                    </span>
+                                    <Image className={Styles.learnMoreLinkIcon} src={'/fonts/icons/arrow-right.svg'}
+                                           height={20} width={20}/>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <Image width={1920} height={1440}
+                       className={Styles.northernInuitDogImage}
+                       src={'/images/inuit-dog/northern-inuit-dog-beach.jpeg'}
+                       alt={'Northern Inuit dog strand'}/>
+
             </div>
+
         </div>
     );
 }
