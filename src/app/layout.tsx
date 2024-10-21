77e4c43a68b6ac5b78d5770bfd27c87d411543da
Robin Chung <robinchung@me.com>
2024-10-21 19:21:31 +0200

    1.0 launch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,18 @@ import Styles from './page.module.scss';
 import GlobalNavbar from "@/layout/desktop-navbar/global-navbar";
 import GlobalFooter from "@/layout/footer/global-footer";
 import MobileNavbar from "@/layout/mobile-navbar/mobile-navbar";
+import { GoogleTagManager } from '@next/third-parties/google'
+
+
+const tagManagerArgs = {
+    gtmId: 'GTM-N4V9NTB9'
+}
 
 const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
     title: "Walkedo | Hondenservice",
-    description: "Het beste adres voor je hond",
+    description: "Walkedo is een alles omvattende hondenservice in Arnhem Noord. Laat je hond uitlaten, vind opvang voor je hond, of cast een wolf lookalike hond voor op de foto of video.",
 };
 
 export default function RootLayout({
@@ -29,7 +35,12 @@ export default function RootLayout({
             <meta name="msapplication-TileColor" content="#da532c"/>
             <meta name="theme-color" content="#ffffff"/>
         </head>
+        <GoogleTagManager gtmId={tagManagerArgs.gtmId} />
         <body className={inter.className}>
+        <noscript>
+            <iframe src={`https://www.googletagmanager.com/ns.html?id=${tagManagerArgs.gtmId}`}
+                    height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe>
+        </noscript>
         <GlobalNavbar className={Styles.desktopNav}/>
         <MobileNavbar className={Styles.mobileNav}/>
         <main className={Styles.main}>
