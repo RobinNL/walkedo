561eb72d1dce3140fe33528ad333a0e467130b33
Robin Chung <robinchung@me.com>
2024-10-21 19:39:27 +0200

    www. redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            // fallback
+            {
+                source: '/:path*',
+                has: [{ type: 'host', value: 'www.walkedo.com' }],
+                destination: 'https://walkedo.com/:path*',
+                permanent: true
+            },
+        ]
+    }
+};
 
 export default nextConfig;
