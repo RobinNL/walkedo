117009d0c437468cb1369828c4e088bc1ef51108
Robin Chung <robinchung@me.com>
2026-01-12 20:30:54 +0100

    redirect to walkedo.com

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,33 @@ const nextConfig = {
     reactStrictMode: true,
     async redirects() {
         return [
-            // fallback
+            // Walkedo.nl redirects
+            {
+                source: '/:path*',
+                has: [{ type: 'host', value: 'walkedo.nl' }],
+                destination: 'https://walkedo.com/:path*',
+                permanent: true
+            },
+            {
+                source: '/:path*',
+                has: [{ type: 'host', value: 'www.walkedo.nl' }],
+                destination: 'https://walkedo.com/:path*',
+                permanent: true
+            },
+            // hondenuitlaatservicearnhem.com redirects
+            {
+                source: '/:path*',
+                has: [{ type: 'host', value: 'hondenuitlaatservicearnhem.com' }],
+                destination: 'https://walkedo.com/:path*',
+                permanent: true
+            },
+            {
+                source: '/:path*',
+                has: [{ type: 'host', value: 'www.hondenuitlaatservicearnhem.com' }],
+                destination: 'https://walkedo.com/:path*',
+                permanent: true
+            },
+            // always without www.
             {
                 source: '/:path*',
                 has: [{ type: 'host', value: 'www.walkedo.com' }],
